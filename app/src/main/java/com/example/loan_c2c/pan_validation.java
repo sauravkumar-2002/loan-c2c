7a85726de698bf5ed35c32efce15c79e802b11ef
Sandeep-0404 <sandeepgh2002@gmail.com>
2022-05-10 13:32:24 +0530

    pan Vaidation

--- app/src/main/java/com/example/loan_c2c/pan_validation.java
+++ app/src/main/java/com/example/loan_c2c/pan_validation.java
@@ -1,6 +1,25 @@
 package com.example.loan_c2c;
 
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class pan_validation {
+
+    public static boolean isValidPanCardNo(String panCardNo)
+    {
+        String regex = "[A-Z]{5}[0-9]{4}[A-Z]{1}";
+        Pattern p = Pattern.compile(regex);
+
+
+        if (panCardNo == null)
+        {
+            return false;
+        }
+
+        Matcher m = p.matcher(panCardNo);
+
+        return m.matches();
+    }
 
 }
